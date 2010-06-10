479c52562567abe33e31a0c70038c3aa2ab7c073
gotrek.mlf <gotrek.mlf@1c276790-0641-1d9d-0ac6-e681f51f2778>
2010-06-10 18:13:41 +0000

    Cambiado método de relleno a PAD Method 1

---
+++ AESphere/src/aesphere/BlockManager.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aesphere;
+
+/**
+ *
+ * @author admin
+ */
+public class BlockManager {
+    
+    private byte [] key;
+    private int keySize;
+    private int blockSize;
+    
+    public BlockManager (byte [] clave, int claveTam, int bloqueTam) {
+        key = clave;
+        keySize = claveTam;
+        blockSize = bloqueTam;
+    }
+
+    public byte [] ECB (byte [] in, String mode)
+            throws IllegalArgumentException {
+        byte [] outBlock = new byte [blockSize];
+
+        int numBlocks = in.length / blockSize;
+        if ( (in.length % blockSize) != 0) numBlocks++;
+
+        byte [] out = new byte [numBlocks * blockSize];
+
+        for (int i = 0; i <= numBlocks; i++) {
+            byte [] inBlock = getBlock (in, i);
+
+            if (mode.equals("cipher")) {
+
+                AESencrypt cifrador = new AESencrypt(key, keySize);
+                cifrador.Cipher(inBlock, outBlock);
+
+            } else if (mode.equals("decipher")) {
+
+                AESdecrypt descifrador = new AESdecrypt(key,keySize);
+                descifrador.InvCipher(inBlock, outBlock);
+
+            } else
+                throw new IllegalArgumentException("Incorrect 'mode' argument");
+
+            out = addBlock (out, outBlock, i);
+        }
+        return out;
+    }
+
+    private byte [] getBlock (byte [] arr, int numBlock) {
+        byte [] aux = new byte [blockSize];
+
+        for ()
+    }
+
+}
