d84717dde992d9860eff5fa6c892f0fe2d625e80
gotrek.mlf <gotrek.mlf@1c276790-0641-1d9d-0ac6-e681f51f2778>
2010-06-15 16:23:15 +0000

    cambiado Print a static

--- AESphere/src/aesphere/Print.java
+++ AESphere/src/aesphere/Print.java
@@ -13,33 +13,22 @@ package aesphere;
 // Print: print arrays of bytes
 public class Print {
    private static final int Nb = 4;
-   private static String[] dig = {"0","1","2","3","4","5","6","7",
-                           "8","9","a","b","c","d","e","f"};
 
-   public Print(){
-
-   }
-
-   // hex: print a byte as two hex digits
-   public static String hex(byte a) {
-      return dig[(a & 0xff) >> 4] + dig[a & 0x0f];
-   }
-
-   public String printArray(String name, byte[] a) {
+   public static String printArray(String name, byte[] a) {
       String cadena=new String();
       cadena= (name + " ");
       for (int i = 0; i < a.length; i++)
-         cadena= cadena + hex(a[i]) + " \n";
+         cadena= cadena + Conversor.byteToHexPair(a[i]) + " \n";
 
       return cadena;   
    }
 
-   public String printArray(String name, byte[][] s) {
+   public static String printArray(String name, byte[][] s) {
       String cadena2 = new String();
       cadena2 = (name + " ");
       for (int c = 0; c < Nb; c++)
          for (int r = 0; r < 4; r++)
-            cadena2 = cadena2 + hex(s[r][c]) + " \n";
+            cadena2 = cadena2 + Conversor.byteToHexPair(s[r][c]) + " \n";
             
       return cadena2;
    }
