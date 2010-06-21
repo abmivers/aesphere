338689df624e546ae2d86f02d551b4374e0c941b
gotrek.mlf <gotrek.mlf@1c276790-0641-1d9d-0ac6-e681f51f2778>
2010-06-21 15:02:10 +0000

    versión alfa de CBC completada

--- AESphere/src/aesphere/BlockManager.java
+++ AESphere/src/aesphere/BlockManager.java
@@ -15,11 +15,13 @@ public class BlockManager {
     private int keySize;
     private int blockSize;
     private String resultado;
+    private byte [] IV;
     
     public BlockManager (byte [] clave, int claveTam, int bloqueTam) {
         key = clave;
         keySize = claveTam;
         blockSize = bloqueTam;
+        IV = new byte[blockSize];
     }
 
     public String getResultado () {
@@ -42,12 +44,13 @@ public class BlockManager {
         if ( (in.length % blockSize) != 0) numBlocks++;
 
         byte [] out = new byte [numBlocks * blockSize];
+        byte [] inBlock = null;
 
         if (cifrando) {
             AESencrypt cifrador = new AESencrypt(key, keySize);
             
             for (int i = 0; i < numBlocks; i++) {
-                byte [] inBlock = getBlock (in, i);
+                inBlock = getBlock (in, i);
                 cifrador.Cipher(inBlock, outBlock);
                 out = addBlock (out, outBlock, i);
             }
@@ -57,7 +60,7 @@ public class BlockManager {
             AESdecrypt descifrador = new AESdecrypt(key,keySize);
 
             for (int i = 0; i < numBlocks; i++) {
-                byte [] inBlock = getBlock (in, i);
+                inBlock = getBlock (in, i);
                 descifrador.InvCipher(inBlock, outBlock);
                 out = addBlock (out, outBlock, i);
             }
@@ -85,8 +88,52 @@ public class BlockManager {
         return out;
     }
 
-    /*public byte [] CBC (byte [] in, String mode)
-            throws IllegalArgumentException {
+    public byte [] CBC (byte [] in, boolean cifrando) {
         byte [] outBlock = new byte [blockSize];
-    }*/
+
+        int numBlocks = in.length / blockSize;
+        if ( (in.length % blockSize) != 0) numBlocks++;
+
+        byte [] out = new byte [numBlocks * blockSize];
+        byte [] inBlock = null;
+
+        if (cifrando) {
+            AESencrypt cifrador = new AESencrypt(key, keySize);
+
+            for (int i = 0; i < numBlocks; i++) {
+                inBlock = getBlock (in, i);
+
+                if (i==0) inBlock = XOR(IV, inBlock);
+                else inBlock = XOR(outBlock, inBlock);
+
+                cifrador.Cipher(inBlock, outBlock);
+                out = addBlock (out, outBlock, i);
+            }
+
+            resultado = cifrador.getCadena();
+        } else {
+            AESdecrypt descifrador = new AESdecrypt(key,keySize);
+
+            for (int i = 0; i < numBlocks; i++) {
+                inBlock = getBlock (in, i);
+
+                if (i==0) inBlock = XOR(IV, inBlock);
+                else inBlock = XOR(outBlock, inBlock);
+
+                descifrador.InvCipher(inBlock, outBlock);
+                out = addBlock (out, outBlock, i);
+            }
+        }
+
+        return out;
+    }
+
+    private byte [] XOR (byte [] a, byte [] b) {
+        byte [] aux = new byte[a.length];
+
+        for (int i=0; i < a.length; i++)
+            aux[i] = (byte) (a[i] ^ b[i]);
+
+        return aux;
+    }
 }
